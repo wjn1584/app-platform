6c267494cd99ecd8ae49037a9c672e9029aba0b8
陈潇文 <chenxiaowen17@huawei.com>
2025-04-09 14:36:45 +0800

    [frontend] 增加modelEngine与插件页面的通信传参

--- app-engine/frontend/src/pages/chatPreview/chatComminPage.tsx
+++ app-engine/frontend/src/pages/chatPreview/chatComminPage.tsx
@@ -6,16 +6,23 @@
  *--------------------------------------------------------------------------------------------*/
 
 import qs from 'qs';
-import React, { useEffect, useMemo } from "react";
+import React, { useEffect, useMemo, useRef } from "react";
 import { useParams, useHistory } from 'react-router-dom';
 import useSearchParams from "@/shared/hooks/useSearchParams";
 import usePlugin from "@/shared/hooks/usePlugin";
 import { AippContext } from '../aippIndex/context';
 import ChatPreview from './index';
+import { useAppSelector } from "@/store/hook";
+import { TENANT_ID } from "@/pages/chatPreview/components/send-editor/common/config";
+import { findConfigValue } from "@/shared/utils/common";
 
 // 公共参数，公共聊天界面
 const CommonChat = (props: any) => {
   const { contextProvider, previewBack, showElsa } = props;
+  const iframeRef = useRef<HTMLIFrameElement | null>(null);
+  const appInfo = useAppSelector((state) => state.appStore.appInfo);
+  const isDebug = useAppSelector((state) => state.commonStore.isDebug);
+  const appRef = useRef<any>({});
 
   const history = useHistory();
 
@@ -68,12 +75,33 @@ const CommonChat = (props: any) => {
         return () => window.removeEventListener('message', handler);
     });
 
+    // 给iframe的对话界面传递参数
+    const sendMessageToIframe = () => {
+      const appInfo = appRef.current;
+      let params = {
+        tenantId: TENANT_ID,
+        appId: appInfo.id,
+        useMemory: findConfigValue(appInfo, 'memory').memorySwitch,
+        isDebug
+      }
+      iframeRef.current?.contentWindow.postMessage({...params}, '*');
+    }
+
+  useEffect(() => {
+    if (Object.keys(appInfo).length === 0) {
+      return;
+    }
+    appRef.current = appInfo;
+  }, [appInfo]);
+
   return (
     (plugin && !showElsa)
     ? <iframe
+        ref={iframeRef}
         src={iframeUrl}
         style={ {border: 'none', height: '100%'} }
         width="100%"
+        onLoad={sendMessageToIframe}
       />
     : <AippContext.Provider value={{ ...contextProvider }}>
         <ChatPreview previewBack={previewBack} />
